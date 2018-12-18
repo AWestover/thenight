fecf06dbfc05c6ef80780f6518b20997b9457b27
Alek Westover <alek.westover@gmail.com>
2018-12-18 18:46:38 -0500

    radii change

--- public/code/constants.js
+++ public/code/constants.js
@@ -12,6 +12,6 @@ let last_down = [0,0];
 
 const keyCodes = {"a":65, "d": 68, "s": 83, "w": 87};
 const userOffset = 10; // the user is this many units behind the sight cone
-const userStillRadius = 250; // radius of circle when not moving
-const userMoveRadius = 500; // radius of the sector shown when moving
+const userStillRadius = 200; // radius of circle when not moving
+const userMoveRadius = 700; // radius of the sector shown when moving
 const userMoveTheta = Math.PI/8; // cone is plus or minus this from angle
